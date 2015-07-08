b9b8278c3593d2f2f6dd1d93b2c6cb373824ae86
Michael Pilquist <mpilquist@gmail.com>
2015-07-08 10:07:36 -0400

    Upgrade to Scala 2.11.7

--- src/main/scala/scodec/build/ScodecBuildSettings.scala
+++ src/main/scala/scodec/build/ScodecBuildSettings.scala
@@ -80,8 +80,8 @@ object ScodecBuildSettings extends AutoPlugin {
   )
 
   private def scalaSettings = Seq(
-    scalaVersion := "2.11.6",
-    crossScalaVersions := Seq("2.11.6", "2.10.5"),
+    scalaVersion := "2.11.7",
+    crossScalaVersions := Seq("2.11.7", "2.10.5"),
     scalacOptions ++= Seq(
       "-deprecation",
       "-encoding", "UTF-8",

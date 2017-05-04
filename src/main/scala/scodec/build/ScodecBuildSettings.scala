6c6121742cf0a46b94fe349779a1a8e6cc584501
xuwei-k <6b656e6a69@gmail.com>
2017-05-04 19:20:44 +0900

    add Scala 2.13.0-M1

--- src/main/scala/scodec/build/ScodecBuildSettings.scala
+++ src/main/scala/scodec/build/ScodecBuildSettings.scala
@@ -81,7 +81,7 @@ object ScodecBuildSettings extends AutoPlugin {
 
   private def scalaSettings = Seq(
     scalaVersion := "2.11.8",
-    crossScalaVersions := Seq("2.11.8", "2.10.6", "2.12.1"),
+    crossScalaVersions := Seq("2.11.8", "2.10.6", "2.12.1", "2.13.0-M1"),
     scalacOptions ++= Seq(
       "-deprecation",
       "-encoding", "UTF-8",
